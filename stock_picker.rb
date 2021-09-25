c24295941bb39a26584c1f8f8ce20c59077fe475
Lucas Furlanetto Pascoali <luccafpascoali@gmail.com>
2021-09-24 21:00:04 -0300

    Add files via upload

---
+++ stock_picker.rb
@@ -0,0 +1,16 @@
+def stock_picker(stock_prices)
+  profit = 0
+  days = []
+
+  stock_prices.each_with_index do |price, day|
+    for i in day..stock_prices.length - 1
+      if stock_prices[i] - price > profit
+        profit = stock_prices[i] - price
+        days = [day, i]
+      end
+    end
+  end
+  days
+end
+
+p stock_picker([32,3,6,9,15,8,6,21,1])
